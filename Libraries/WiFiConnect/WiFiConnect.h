0b4896d9e1bcbb073ff52fc91948b0c55471e874
florianfuehner <32961890+florianfuehner@users.noreply.github.com>
2017-10-29 18:40:12 +0000

    Create WiFiConnect.h

---
+++ Libraries/WiFiConnect/WiFiConnect.h
@@ -0,0 +1,26 @@
+/*
+Headerfile for WiFiConnect.ino
+For further documention for the functions have a look at the comments of the .ino file. 
+The variables declared as private are not reachable from your program. 
+The variables declared as public are reachable from your program. 
+*/
+
+#ifndef WiFiConnect_h
+#define WiFiConnect_h
+
+#include <Arduino.h>
+#include <WiFiConnect.ino> // included as link to the function. 
+#include <WiFi.h>
+#include <WiFiServer.h>
+
+class WiFiConnection {
+	private:
+		int connectTry;
+		const char* _ssid;
+		const char* _password;
+	public:
+		WiFiServer server; // creates the instance server of WiFiServer
+		void WiFiConnect() { }; //Prototype of function
+};
+
+#endif
